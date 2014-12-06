77a8d0e89ac00f855c83d50544db8c85b82c2326
Jonah Stiennon <jonahss@gmail.com>
2014-12-05 16:42:58 -0800

    first test, moved test dir

--- test/specs.js
+++ test/specs.js
@@ -8,4 +8,8 @@ describe('chromedriver', () => {
   it('should exist', () => {
     should.exist(Chromedriver);
   });
+
+  it('should be able to get path to chromedriver executable', () => {
+    should.exist(Chromedriver.getPath());
+  });
 });
