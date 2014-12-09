77a80a446ec274009f8970ec1c71c914d1d8f3b0
Jonathan Lipps <jlipps@gmail.com>
2014-12-09 15:53:12 -0800

    initial tests and scratch

--- test/specs.js
+++ test/specs.js
@@ -1,17 +1,47 @@
 // transpile:mocha
+/* global it:true, describe:true, before:true */
 
 import { default as Chromedriver } from '../lib/chromedriver';
 import chai from 'chai';
-import 'mochawait';
+import Q from 'q';
+//let cbIt = it;
+//import 'mochawait';
 
 chai.should();
 
-describe('chromedriver', () => {
-  it('should exist', () => {
-    Chromedriver.should.exist;
-  });
+async function nextState (cd) {
+  let d = Q.defer();
+  cd.on('stateChanged', d.resolve);
+  return d.promise;
+}
 
-  it('should be able to get path to chromedriver executable', () => {
-    Chromedriver.getPath().should.exist;
+async function assertNextState (cd, state) {
+  let msg = await nextState(cd);
+  msg.state.should.equal(state);
+}
+
+async function assertNoRunningChromedrivers () {
+}
+
+describe('chromedriver', () => {
+  let cd = null;
+  const caps = {browserName: 'chrome'};
+  before(async () => {
+    let opts = {};
+    cd = new Chromedriver(opts);
+    await assertNoRunningChromedrivers();
+  });
+  it('should start a session', async () => {
+    cd.state.should.eql('stopped');
+    cd.start(caps);
+    cd.capabilities.should.eql(caps);
+    await assertNextState(cd, 'starting');
+    await assertNextState(cd, 'online');
+  });
+  it('should stop a session', async () => {
+    cd.stop();
+    await assertNextState(cd, 'stopping');
+    await assertNextState(cd, 'stopped');
+    await assertNoRunningChromedrivers();
   });
 });
