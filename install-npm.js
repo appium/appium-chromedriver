cd67b14369485ffb62e5506269f346a0ca65a641
Isaac Murchie <isaac@saucelabs.com>
2017-08-16 11:26:50 -0400

    Fix install script for node 4

--- install-npm.js
+++ install-npm.js
@@ -9,7 +9,7 @@ const path = require('path');
 function waitForDeps (cb) {
   // see if we can import the necessary code
   // try it a ridiculous (but finite) number of times
-  let i = 0;
+  var i = 0; // eslint-disable-line no-var
   function check () {
     i++;
     try {
@@ -33,7 +33,7 @@ function waitForDeps (cb) {
 
 if (require.main === module) {
   // check if cur dir exists
-  let installScript = path.resolve(__dirname, 'build', 'lib', 'install.js');
+  var installScript = path.resolve(__dirname, 'build', 'lib', 'install.js'); // eslint-disable-line no-var
   waitForDeps(function (err) {
     if (err) {
       console.warn("Unable to import install script. Re-run `install appium-chromedriver` manually.");
