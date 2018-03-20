5f85ffbd5704a8b1f5f585b79aa1c2ccc4cf6802
Isaac Murchie <isaac@saucelabs.com>
2018-03-20 13:06:10 -0400

    Allow multiple chromedriver versions to be available

--- install-npm.js
+++ install-npm.js
@@ -9,15 +9,15 @@ const path = require('path');
 function waitForDeps (cb) {
   // see if we can import the necessary code
   // try it a ridiculous (but finite) number of times
-  var i = 0; // eslint-disable-line no-var
+  let i = 0;
   function check () {
     i++;
     try {
       require('./build/lib/install');
       cb();
     } catch (err) {
-      if (err.message.indexOf("Cannot find module './build/lib/install'") !== -1) {
-        console.warn('Project does not appear to built yet. Please run `gulp transpile` first.');
+      if (err.message.includes("Cannot find module './build/lib/install'")) {
+        console.warn('Project does not appear to be built yet. Please run `gulp transpile` first.');
         return cb('Could not install module: ' + err);
       }
       console.warn('Error trying to install Chromedriver binary. Waiting and trying again.', err.message);
@@ -33,7 +33,7 @@ function waitForDeps (cb) {
 
 if (require.main === module) {
   // check if cur dir exists
-  var installScript = path.resolve(__dirname, 'build', 'lib', 'install.js'); // eslint-disable-line no-var
+  let installScript = path.resolve(__dirname, 'build', 'lib', 'install.js');
   waitForDeps(function (err) {
     if (err) {
       console.warn("Unable to import install script. Re-run `install appium-chromedriver` manually.");
