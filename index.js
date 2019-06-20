559b08307b454fd228e3ff392213485d91a88331
Mykola Mokhnach <mokhnach@gmail.com>
2019-06-20 19:55:07 +0200

    Add a helper for chromedrivers download from Google storage

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 import { default as chromedriver } from './lib/chromedriver';
+import ChromedriverStorageClient from './lib/storage-client';
 
+export { ChromedriverStorageClient };
 export default chromedriver;
