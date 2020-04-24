89db7cab8bd791b1d89a3962923dc90df7229ad8
Mykola Mokhnach <mokhnach@gmail.com>
2020-04-24 08:27:43 +0200

    Update the script

--- scripts/handle_chromedriver_feed.py
+++ scripts/handle_chromedriver_feed.py
@@ -1,24 +1,19 @@
-from typing import List
+from typing import List, Dict
 import sys
 import json
 
 
-def extract_known_versions(json_path: str) -> List[str]:
+def main(latest_version: str, json_path: str) -> None:
     with open(json_path, 'r') as f:
-        versions_map = json.load(f)
-    return list(versions_map.keys())
+        known_versions: Dict[str, str] = json.load(f)
+    if latest_version in known_versions.keys():
+        return
 
-
-def write_new_version(json_path: str, version: str) -> None:
-    result = {version: version}
-    with open(json_path, 'r') as f:
-        result.update(json.load(f))
+    result = {latest_version: latest_version}
+    result.update(known_versions)
     with open(json_path, 'w') as f:
         json.dump(result, f, indent=2)
 
 
 if __name__ == '__main__':
-    latest_version = sys.argv[1].strip()
-    known_versions = extract_known_versions(sys.argv[2])
-    if latest_version not in known_versions:
-        write_new_version(sys.argv[2], latest_version)
+    main(sys.argv[1].strip(), sys.argv[2])
