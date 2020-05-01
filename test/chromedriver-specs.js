0175f76b2f171b7689f14b0097c00ddcb21cf180
Mykola Mokhnach <mokhnach@gmail.com>
2020-05-01 15:49:52 +0200

    refactor: Make chromedriver CDN url configurable

--- test/chromedriver-specs.js
+++ test/chromedriver-specs.js
@@ -1,4 +1,4 @@
-import { Chromedriver, getMostRecentChromedriver, CHROMEDRIVER_CHROME_MAPPING } from '../lib/chromedriver';
+import { Chromedriver } from '../lib/chromedriver';
 import * as utils from '../lib/utils';
 import sinon from 'sinon';
 import chai from 'chai';
@@ -157,7 +157,7 @@ describe('chromedriver', function () {
               stdout: 'ChromeDriver 2.30.540469 (1881fd7f8641508feb5166b7cae561d87723cfa8)',
             });
 
-        const chromedrivers = await cd.getChromedrivers(CHROMEDRIVER_CHROME_MAPPING);
+        const chromedrivers = await cd.getChromedrivers(utils.CHROMEDRIVER_CHROME_MAPPING);
         for (const [cd, expectedVersion] of _.zip(chromedrivers, ['74.0.3729.6', '2.36', '2.35', '2.34', '2.33', '2.32', '2.31', '2.30'])) {
           cd.version.should.eql(expectedVersion);
           cd.minChromeVersion.should.to.not.be.null;
@@ -270,7 +270,7 @@ describe('chromedriver', function () {
 
   describe('getMostRecentChromedriver', function () {
     it('should get a value by default', function () {
-      getMostRecentChromedriver().should.be.a.string;
+      utils.getMostRecentChromedriver().should.be.a.string;
     });
     it('should get the most recent version', function () {
       const mapping = {
@@ -282,7 +282,7 @@ describe('chromedriver', function () {
         '2.7': '30.0.1573',
         '2.6': '29.0.1545',
       };
-      getMostRecentChromedriver(mapping).should.eql('2.12');
+      utils.getMostRecentChromedriver(mapping).should.eql('2.12');
     });
     it('should handle broken semver', function () {
       const mapping = {
@@ -294,10 +294,10 @@ describe('chromedriver', function () {
         '2.7': '30.0.1573',
         '2.6': '29.0.1545',
       };
-      getMostRecentChromedriver(mapping).should.eql('2.12');
+      utils.getMostRecentChromedriver(mapping).should.eql('2.12');
     });
     it('should fail for empty mapping', function () {
-      (() => getMostRecentChromedriver({}))
+      (() => utils.getMostRecentChromedriver({}))
         .should.throw('Unable to get most recent Chromedriver from empty mapping');
     });
   });
