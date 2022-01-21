07fb1da0bc591d40f5a7287c588cd7c34f08aa2f
Mykola Mokhnach <mokhnach@gmail.com>
2022-01-21 11:42:06 +0100

    refactor!: Switch the driver to use Appium2 modules (#238)

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const gulp = require('gulp');
-const boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
-const DEFAULTS = require('appium-gulp-plugins').boilerplate.DEFAULTS;
+const boilerplate = require('@appium/gulp-plugins').boilerplate.use(gulp);
+const DEFAULTS = require('@appium/gulp-plugins').boilerplate.DEFAULTS;
 
 boilerplate({
   build: 'Appium Chromedriver',
