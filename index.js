0cf1d53fb1dceee5b34d3298cb092a7b6b8ff382
Christopher Hiller <boneskull@boneskull.com>
2023-02-15 12:50:06 -0800

    feat: add types
    This compiles the project with `tsc` and publishes type declarations.
    
    - All of `lib` is typed. some typing bugs were corrected
    - types for `appium-adb` are stubbed, since that package also needs types
    - `test` is untyped b/c it's a wreck
    - removes babel
    - upgrades eslint
    - removes `pre-commit` for `husky`

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ import ChromedriverStorageClient from './lib/storage-client';
 
 export { ChromedriverStorageClient };
 export default chromedriver;
+
+/**
+ * @typedef {import('./lib/types').ChromedriverOpts} ChromedriverOpts
+ * @typedef {import('./lib/types').ChromedriverDetails} ChromedriverDetails
+ * @typedef {import('./lib/types').ChromedriverVersionMapping} ChromedriverVersionMapping
+ * @typedef {import('./lib/types').ChromedriverStorageClientOpts} ChromedriverStorageClientOpts
+ * @typedef {import('./lib/types').SyncOptions} SyncOptions
+ * @typedef {import('./lib/types').OSInfo} OSInfo
+ */
