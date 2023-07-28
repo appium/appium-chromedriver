14662336ebd7c6f425a9bca8d1b78c9dee56d6e1
Mykola Mokhnach <mokhnach@gmail.com>
2023-07-28 22:10:50 +0200

    Tune tests

--- test/functional/storage-client-e2e-specs.js
+++ test/functional/storage-client-e2e-specs.js
@@ -41,8 +41,9 @@ describe('ChromedriverStorageClient', function () {
     });
     try {
       (await client.syncDrivers({
-        versions: ['115.0.5790.102', '115.0.5739.0'],
+        versions: ['115.0.5790.102', '114.0.5696.0'],
       })).length.should.be.greaterThan(0);
+      console.log(JSON.stringify(client.mapping, null, 2));
       (await fs.readdir(tmpRoot)).length.should.be.eql(2);
     } finally {
       await fs.rimraf(tmpRoot);
