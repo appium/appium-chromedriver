1d363d8d1a2fcf544bab953b0c5f62df9ad6651a
Mykola Mokhnach <mokhnach@gmail.com>
2023-07-29 08:41:50 +0000

    ci: Update the latest version retrieval logic (#334)

---
+++ scripts/update_versions_mapping.js
@@ -0,0 +1,29 @@
+const fs = require('fs/promises');
+const _ = require('lodash');
+
+async function main () {
+  const [latestVersion, jsonPath] = process.argv.slice(2);
+  if (!latestVersion) {
+    throw new Error(
+      'The latest Chromedriver version must be provided as the first command line argument'
+    );
+  }
+  if (!jsonPath) {
+    throw new Error(
+      'The path to the versions mapping json must be provided as the second command line argument'
+    );
+  }
+
+  const json = JSON.parse(await fs.readFile(jsonPath, 'utf8'));
+  if (latestVersion in json) {
+    process.stdout.write(0);
+    return;
+  }
+
+  const pairs = _.toPairs(json);
+  pairs.unshift([latestVersion, latestVersion]);
+  await fs.writeFile(jsonPath, JSON.stringify(_.fromPairs(pairs), null, 2), 'utf8');
+  process.stdout.write(1);
+}
+
+(async () => await main())();
